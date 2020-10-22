212cb4914387d763a8b6c0acf6414a00c8da39f1
chrisgs <chrisgs16@hotmail.com>
2020-10-22 16:05:16 +0100

    Update demo script again

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 print_message('This is a demo script')
 print_message('Modification made')
+print_message('Another modification made')
 
 def print_message(message: str):
     print('Message: {}'.format(message))
